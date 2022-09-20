00654b59e5669998f635cd2021fa50acc11c309d
Francis Jacob Taiño <francistaino16@gmail.com>
2022-09-21 03:55:12 +0800

    Added app.js file.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    port = 3000 || process.env.PORT;
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, function(){
+    console.log("Server started on port " + port);
+})

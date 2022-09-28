40e4f3ecf19911e535a90df30aa88d691e099e24
Francis Jacob Taiño <francistaino16@gmail.com>
2022-09-29 07:36:28 +0800

    Added insert products to db.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 const port = 3000 || process.env.PORT;
@@ -23,6 +24,7 @@ async function main(){
     console.log("Connected to db!");
 }
 
+//Schemas
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -49,7 +51,17 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = new mongoose.model("User", userSchema)
+const productSchema = new mongoose.Schema({
+    brand: String,
+    name: String,
+    price: Number,
+    description: String,
+    quantity: Number
+});
+
+//Models
+const User = new mongoose.model("User", userSchema);
+const Product = new mongoose.model("Product", productSchema);
 
 app.get("/", function(req, res){
     res.render('index');
@@ -81,16 +93,18 @@ app.get("/login", function(req, res){
 
 app.post("/login", function(req, res){
     const userEmail = req.body.email;
-    const userPassword = md5(req.body.password);
+    const userPassword = req.body.password;
     
     User.findOne({email: userEmail}, function(err, foundUser){
         if(err){
             console.log(err);
         } else {
             if(foundUser){
-                if(foundUser.password === userPassword){
-                    res.render('admin/dashboard');
-                }
+                bcrypt.compare(userPassword, foundUser.password, function(err, result){
+                    if(result === true){
+                        res.render('admin/dashboard');
+                    }
+                });
             }
         }});
 });
@@ -100,28 +114,53 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const userFirstName = req.body.firstName;
-    const userLastName = req.body.lastName;
-    const userEmail = req.body.email;
-    const userPassword = req.body.password;
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const user = new User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: hash,
+            accountType: "user"
+        });
+    
+        user.save(function(err){
+            if(err){
+                console.log(err);
+            } else {
+                res.render('admin/dashboard');
+            }
+        });
+    })
+    
 
-    const user = new User({
-        firstName: userFirstName,
-        lastName: userLastName,
-        email: userEmail,
-        password: md5(userPassword),
-        accountType: "user"
+});
+
+app.get("/admin/products", function(req, res){
+    Product.find({}, function(err, foundProducts){
+    res.render("admin/products", newListItems: foundItems});
+}
+
+app.get("/admin/product/add", function(req, res){
+    res.render('admin/add-product');
+});
+
+app.post("/addProduct", function(req, res){
+    const product = new Product({
+        brand: req.body.productBrand,
+        name: req.body.productName,
+        price: req.body.productPrice,
+        description: req.body.productDescription,
+        quantity: req.body.productQuantity
     });
 
-    user.save(function(err){
+    product.save(function(err){
         if(err){
             console.log(err);
         } else {
-            res.render('admin/dashboard');
+            res.render('admin/products');
         }
-    });
-
-});
+    })
+})
 
 
 app.listen(port, function(){

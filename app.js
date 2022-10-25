62308a12b29f3a0dfac450cc3e4d9f77d482cbe0
Francis Jacob Taiño <francistaino16@gmail.com>
2022-10-25 19:07:23 +0800

    Added routes for account functionalities and products.

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ const randomstring = require("randomstring");
 const app = express();
 const port = 3000 || process.env.PORT;
 
-//EXPORT MODELS
-const User = require("./models/userModel");
-const Product = require("./models/productModel");
+
 
 //FOR IMAGE DISPLAY
 var fs = require('fs');
@@ -46,6 +44,16 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+//EXPORT MODELS
+const User = require("./models/userModel");
+const Product = require("./models/productModel");
+
+//EXPORT ROUTES
+const productRoute = require("./routes/Products");
+app.use("/products", productRoute);
+
+const accountRoute = require("./routes/Account");
+app.use("/account", accountRoute);
 
 //MongoDB
 main().catch(err => console.log(err));
@@ -61,250 +69,15 @@ app.get("/", function(req, res){
     res.render('index');
 });
 
-//PRODUCTS
-app.get("/onhand", function(req, res){
-    Product.find({type:"On-Hand"}, function (err, allProducts) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("onhand", { newOnHandProducts: allProducts })
-        }
-    });
-});
-
-app.get("/preorder", function(req, res){
-    Product.find({type:"Pre-Order"}, function (err, allProducts) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("preorder", { newPreOrderProducts: allProducts })
-        }
-    });
-});
-
-app.get("/accessories", function(req, res){
-    Product.find({type:"Accessories"}, function (err, allProducts) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("accessories", { newAccessoriesProducts: allProducts })
-        }
-    });
-});
-
-app.get("/apparel", function(req, res){
-    Product.find({type:"Apparel"}, function (err, allProducts) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("apparel", { newApparelProducts: allProducts })
-        }
-    });
-});
-
 app.get("/about", function(req, res){
     res.render('about');
 });
 
 //LOGIN/REGISTER/FORGOT PASSWORD
-app.get("/login", function(req, res){
-    res.render('login');
-});
 
-app.post("/login", function(req, res){
-    const userEmail = req.body.email;
-    const userPassword = req.body.password;
-    
-    User.findOne({email: userEmail}, function(err, foundUser){
-        if(err){
-            console.log(err);
-        } else {
-            if(foundUser){
-                bcrypt.compare(userPassword, foundUser.password, function(err, result){
-                    if(result === true){
-                        if(foundUser.isVerified === "false"){
-                            res.render('login', {message: "Please check and verify your account in your email."});
-                        }
-                        else if(foundUser.accountType === "admin"){
-                            // req.session.userId = foundUser._id;
-                            res.render('admin/dashboard');
-                        }
-                        else{
-                            // req.session.userId = foundUser._id;
-                            res.render('index');
-                        }
-                    }
-                    else{
-                        res.render('login', {message: "Incorrect login credentials!"});
-                    }
-                });
-            }
-            else{
-                res.render('login', {message: "Incorrect email and password!"});
-            }
-        }});
-});
-
-
-
-app.get("/register", function(req, res){
-    res.render('register');
-});
-
-app.post("/register", function(req, res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-        const user = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: hash,
-            accountType: "user"
-        });
-    
-        user.save(function(err){
-            if(err){
-                console.log(err);
-            } else {
-                sendVerifyMail(req.body.firstName, req.body.email, user._id);
-                res.render('register', {message: "Your registration has been successful. Please check and verify your account."});
-            }
-        });
-    });
-});
 
 //EMAIL FUNCTIONALITIES
-const sendVerifyMail = async(name, email, user_id) =>{
-    try{
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port:465,
-            secure: true,
-            auth:{
-                user: process.env.SECRETEMAIL,
-                pass: process.env.SECRETPASSWORD
-            },
-            tls:{
-                rejectUnauthorized: false
-            }
-        });
-        
-        const mailOptions= {
-            from: process.env.SECRETEMAIL,
-            to: email,
-            subject: "For Verification",
-            html:'<p>Hi '+name+', please click here to <a href="http://localhost:3000/verify?id='+user_id+'"> Verify </a> your mail</p>'
-        }
 
-        transporter.sendMail(mailOptions, function(error, info){
-            if(error){
-                console.log(error);
-            }
-            else{
-                console.log("Email has been sent:- ", info.response);
-            }
-        });
-    }
-    catch (error){
-        console.log(error);
-    }
-}
-
-app.get("/verify", function(req, res){
-    User.findByIdAndUpdate({_id:req.query.id}, {$set:{isVerified: true}}, function(err, user){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render('email-verified');
-        }
-    });
-});
-
-app.get("/forgot", function(req, res){
-    res.render('forgot');
-});
-
-app.post("/forgot", function(req, res){
-    
-    const forgotEmail = req.body.email;
-    User.findOne({email: forgotEmail}, function(err, foundUser){
-        if(foundUser){
-            if(foundUser.isVerified === "false"){
-                res.render('forgot', {message: "Please check and verify your email."});
-            } else{
-                const randomString = randomstring.generate();
-                User.updateOne({email: forgotEmail}, {$set: {token: randomString}}, function(err, user){});
-                sendResetPasswordMail(foundUser.firstName, foundUser.email, randomString);
-                res.render('forgot', {message: "Please check your email for the reset link of forgotten password."});
-            }
-        } else {
-            res.render('forgot', {message: "User email is incorrect."});
-        }
-    });
-});
-
-const sendResetPasswordMail = async(name, email, token) =>{
-    try{
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port:465,
-            secure: true,
-            auth:{
-                user: process.env.SECRETEMAIL,
-                pass: process.env.SECRETPASSWORD
-            },
-            tls:{
-                rejectUnauthorized: false
-            }
-        });
-        
-        const mailOptions= {
-            from: process.env.SECRETEMAIL,
-            to: email,
-            subject: "For Reset Password",
-            html:'<p>Hi '+name+', please click here to <a href="http://localhost:3000/forget-password?token='+token+'"> Reset </a> your password.</p>'
-        }
-
-        transporter.sendMail(mailOptions, function(error, info){
-            if(error){
-                console.log(error);
-            }
-            else{
-                console.log("Email has been sent:- ", info.response);
-            }
-        });
-    }
-    catch (error){
-        console.log(error);
-    }
-}
-
-app.get("/forget-password", function(req, res){
-    const forgotToken = req.query.token;
-    User.findOne({token: forgotToken}, function(err, foundToken){
-        if(foundToken){
-            res.render('forget-password', {userId: foundToken._id});
-        } else {
-            res.render('404', {message: "Token is invalid"});
-        }
-    });
-});
-
-app.post("/forget-password", function(req, res){
-    const newPassword = req.body.password;
-    const userId = req.body.userId;
-
-    bcrypt.hash(newPassword, saltRounds, function(err, hash){
-        User.findByIdAndUpdate({_id: userId}, {$set: {password: hash}, token:''}, function(err, user){
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.render('login', {message: "Your password is now reset, you can now login."});
-            }
-        });
-    });
-});
 
 //ADMIN DASHBOARD
 app.get("/admin/dashboard", function(req, res){

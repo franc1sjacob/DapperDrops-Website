aad199d032fb3c432cbe493cc62f0fb054b032a9
Samarth Sharma <samarth.sharma.iics@ust.edu.ph>
2022-11-02 12:11:13 +0800

    push added cart items of current user to initialized instance of cart

--- routes/Cart.js
+++ routes/Cart.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require("../models/userModel");
 const Product = require("../models/productModel");
 const Cart = require("../models/cartModel");
 const { ObjectID } = require('bson');
+
+var cart;
 
 const isAuth = function(req, res, next){
     if(req.session.isAuth){
@@ -14,41 +17,37 @@ const isAuth = function(req, res, next){
 }
 
 router.get("/view-cart", isAuth, function(req, res){
-    Cart.find({}, function(err, foundCarts){
+    Cart.findOne({userId: req.session.userId}, function(err, cart){
         if(err){
             console.log(err);
         } else{
-            res.render('view-cart', {userCart:foundCarts});
+            res.render('view-cart', {cart: cart});
         }
     });
 });
 
 router.post("/add-to-cart", function(req, res){
-    const prodId = req.body.prodId; 
-    console.log(prodId);
+    const userId = req.session.userId;
+    const { prodId, variation, quantity} = req.body;
+    console.log(req.body);
     Product.findById(prodId, function(err, foundProduct){
-        if(err){
-            console.log(err);
-        } else{
-            const cart = new Cart({
-                products: [{
-                    productId: foundProduct._id,
-                    name: foundProduct.name,
-                    brand: foundProduct.brand,
-                    category: foundProduct.category,
-                    quantity: 1,
-                    price: foundProduct.price
-                }],
-                total: foundProduct.price
-            });
-            
-            cart.save(function(err){
-                if(err){
-                    console.log(err);
-                }
-                res.redirect('/cart/view-cart');
-            });
+        const product = {
+            productId: foundProduct._id,
+            name: foundProduct.name,
+            brand: foundProduct.brand,
+            category: foundProduct.category,
+            variation: variation,
+            quantity: quantity,
+            price: foundProduct.price,
+            totalPrice: foundProduct.price * quantity
         }
+        Cart.findOneAndUpdate({userId: userId},  {$push: {products: [product]}}, function(err){
+            if(err){
+                console.log(err);
+            } else{
+                res.redirect('/cart/view-cart');
+            }
+        });
     });
 });
 

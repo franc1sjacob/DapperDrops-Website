90748ccc70bcb655473f64bce4a769300053d857
Raymond Reynard Flores <raymondreynard.flores.iics@ust.edu.ph>
2022-12-05 23:10:43 +0800

    mongoUri

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const Featured = require("./models/featuredModel");
 
 //MongoDB
 // const mongoUri = "mongodb+srv://admin-dapperdrops:admin123@cluster0.i5opsug.mongodb.net/dapperdropsDB";
-const mongoUri = "mongodb://localhost:27017/dapperdropsDB";
-// const mongoUri = "mongodb://admin-dapperdrops:admin123@ac-urowtdl-shard-00-00.i5opsug.mongodb.net:27017,ac-urowtdl-shard-00-01.i5opsug.mongodb.net:27017,ac-urowtdl-shard-00-02.i5opsug.mongodb.net:27017/dapperdropsDB?ssl=true&replicaSet=atlas-6r1cqm-shard-0&authSource=admin&retryWrites=true&w=majority"
+// const mongoUri = "mongodb://localhost:27017/dapperdropsDB";
+const mongoUri = "mongodb://admin-dapperdrops:admin123@ac-urowtdl-shard-00-00.i5opsug.mongodb.net:27017,ac-urowtdl-shard-00-01.i5opsug.mongodb.net:27017,ac-urowtdl-shard-00-02.i5opsug.mongodb.net:27017/dapperdropsDB?ssl=true&replicaSet=atlas-6r1cqm-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 main().catch(err => console.log(err));
 
